362a49e21b9f037faf14eb2c195c928493bf3de6
Millioner <millioner.bbb@gmail.com>
2011-05-19 11:30:55 +0300

    Static files including fix

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(name='django-ajax-selects',
     url='',
     packages=['ajax_select', ],
     include_package_data = True,    # include everything in source control
-    package_data={'ajax_select': ['*.py','*.txt','*.css','*.gif','js/*.js','templates/*.html']},
+    package_data={'ajax_select': [
+        '*.py', 'templates/*.html', 'static/css/*.css', 'static/js/*.js', 'static/images/*.gif', 'static/images/*.png'
+    ]},
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
